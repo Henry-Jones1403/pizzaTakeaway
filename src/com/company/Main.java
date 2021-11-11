e0f79077fc662eeebca4032d2c27e39ebe7f3430
Henry-Jones1403 <90896656+Henry-Jones1403@users.noreply.github.com>
2021-11-11 20:44:49 +0000

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,5 @@
 package com.company;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     public static Map<String, Double> pizzaPrice() {
@@ -12,37 +9,63 @@ public class Main {
         pizzaPrice.put("large", 13.99);
         return pizzaPrice;
     }
-
     public static void main(String[] args) {
         String size = "";
         double price = 0.00;
         Map<String, Double> pizzaPrice = pizzaPrice();
+        String[] toppings = {"ham", "cheese", "tomato", "pepperoni", "sausage", "mushroom", "onion", "olives", "garlic", "anchovies"};
         ArrayList<String> allToppings = new ArrayList<>();
         Scanner input = new Scanner(System.in);
+        boolean accepted = false;
+        while(!accepted){
         try{
             System.out.println("What size would you like? ");
             size = input.next();
             price = pizzaPrice.get(size);
             System.out.println(price);
+            accepted = true;
         }catch (NullPointerException e){
             System.out.println("That is not a size " + e);
-            input.next();
+        }catch (Exception e){
+            System.out.println("Sorry there was an error, try again " + e);
 
+        }}
+        int counter = 0;
+        String userToppings = "";
+        System.out.print("At any point type 'end' to finish and pay. These are our toppings: ");
+        for (String topping : toppings) {
+            System.out.print(topping + ", ");
         }
+        System.out.println();
+        while (!userToppings.equals("end")) {
+            boolean found = false;
 
-        String toppings = "";
-        while (!toppings.equals("end")) {
-            System.out.println("What toppings would you like to add");
-            toppings = input.next();
-            if(!toppings.equals("end")){
-                allToppings.add(toppings);
+            if(counter<2) {
+                System.out.println("What free topping would you like to add");
+            }else{
+                System.out.println("What topping would you like to add");
             }
+            userToppings = input.next();
+            if(!userToppings.equals("end")){
+                for (String topping : toppings) {
+                    if (userToppings.equals(topping)) {
+                        allToppings.add(userToppings);
+                        found = true;
+                        break;
+                    }
+                }
+                if(!found){
+                    System.out.println("Try again, that isn't an option.");
+                }
 
+
+            }counter++;
         }
         System.out.println("You chose a " + size + " and have these toppings: ");
-        for (int i = 0; i < allToppings.size(); i++) {
-            System.out.println(allToppings.get(i));
+        for (String allTopping : allToppings) {
+            System.out.println(allTopping);
         }
-        System.out.println("This will cost you £" + (price+(1.25 * allToppings.size())));
+        price = price + (1.25 * (allToppings.size()-2));
+        System.out.println("This will cost you £" + price);
     }
 }
